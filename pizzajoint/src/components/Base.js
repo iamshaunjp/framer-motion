aead25230e6410b475b3d1e866ec4061fdb688d7
Shaun <shaun@thenetninja.co.uk>
2020-04-09 17:59:44 +0100

    lesson-7

--- pizzajoint/src/components/Base.js
+++ pizzajoint/src/components/Base.js
@@ -2,13 +2,36 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
+const containerVariants = {
+  hidden: { 
+    opacity: 0, 
+    x: '100vw' 
+  },
+  visible: { 
+    opacity: 1, 
+    x: 0,
+    transition: { type: 'spring', delay: 0.5 }
+  },
+};
+
+const nextVariants = {
+  hidden: { 
+    x: '-100vw' 
+  },
+  visible: {
+    x: 0,
+    transition: { type: 'spring', stiffness: 120 }
+  } 
+}
+
 const Base = ({ addBase, pizza }) => {
   const bases = ['Classic', 'Thin & Crispy', 'Thick Crust'];
 
   return (
     <motion.div className="base container"
-      initial={{ opacity: 0, x: '100vw' }}
-      animate={{ opacity: 1, x: 0 }}
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
       transition={{ type: 'spring', delay: 0.5 }}
     >
       <h3>Step 1: Choose Your Base</h3>
@@ -28,9 +51,7 @@ const Base = ({ addBase, pizza }) => {
 
       {pizza.base && (
         <motion.div className="next"
-          initial={{ x: '-100vw' }}
-          animate={{ x: 0 }}
-          transition={{ type: 'spring', stiffness: 120 }}
+          variants={nextVariants}
         >
           <Link to="/toppings">
             <motion.button
