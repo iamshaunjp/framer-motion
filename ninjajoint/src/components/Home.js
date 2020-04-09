ffcdd52098929f697fa71ccb995834f6453adb65
Shaun <shaun@thenetninja.co.uk>
2020-04-09 12:18:00 +0100

    lesson-3

--- ninjajoint/src/components/Home.js
+++ ninjajoint/src/components/Home.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const Home = () => {
   return (
-    <div className="home container">
-      <h2>Welcome to Pizza Joint</h2>
+    <motion.div className="home container"
+      animate={{ rotateZ: 180, opacity: 0.2, marginTop: 200 }}
+    >
+      <motion.h2 
+        animate={{ x: 100, fontSize: 50, color: '#ff2994' }}
+      >
+        Welcome to Pizza Joint
+      </motion.h2>
       <Link to="/base">
-        <button>
+        <motion.button
+          animate={{ scale: 1.5 }}
+        >
           Create Your Pizza
-        </button>
+        </motion.button>
       </Link>
-    </div>
+    </motion.div>
   )
 }
 
