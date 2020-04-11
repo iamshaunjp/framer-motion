fd7ae394fb59740286c1d50ea86f61061425168f
Shaun <shaun@thenetninja.co.uk>
2020-04-11 10:31:16 +0100

    lesson-15

--- pizzajoint/src/components/Header.js
+++ pizzajoint/src/components/Header.js
@@ -1,20 +1,50 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
+const svgVariants = {
+  hidden: { rotate: -180 },
+  visible: { 
+    rotate: 0,
+    transition: { duration : 1 }
+  },
+}
+
+const pathVariants = {
+  hidden: {
+    opacity: 0,
+    pathLength: 0,
+  },
+  visible: {
+    opacity: 1,
+    pathLength: 1,
+    transition: { 
+      duration: 2,
+      ease: "easeInOut",
+    }
+  }
+};
+
+
 const Header = () => {
   return (
     <header>
       <div className="logo">
-        <svg className="pizza-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-          <path
+        <motion.svg className="pizza-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"
+          variants={svgVariants}
+          initial="hidden"
+          animate="visible"
+        >
+          <motion.path
             fill="none"
             d="M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z"
+            variants={pathVariants}
           />
-          <path
+          <motion.path
             fill="none"
             d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z"
+            variants={pathVariants}
           />
-        </svg>
+        </motion.svg>
       </div>
       <motion.div className="title"
         initial={{ y: -250}}
