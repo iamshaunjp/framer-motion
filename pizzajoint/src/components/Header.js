0a8e08cf10dc15cba5db661eba6c7ae2ab1bf33d
Shaun <shaun@thenetninja.co.uk>
2020-04-11 12:03:43 +0100

    lesson-18

--- pizzajoint/src/components/Header.js
+++ pizzajoint/src/components/Header.js
@@ -28,7 +28,11 @@ const pathVariants = {
 const Header = () => {
   return (
     <header>
-      <div className="logo">
+      <motion.div className="logo"
+        drag
+        dragConstraints={{ left: 0, top: 0, right: 0, bottom: 0 }}
+        dragElastic={0.7}
+      >
         <motion.svg className="pizza-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"
           variants={svgVariants}
           initial="hidden"
@@ -45,7 +49,7 @@ const Header = () => {
             variants={pathVariants}
           />
         </motion.svg>
-      </div>
+      </motion.div>
       <motion.div className="title"
         initial={{ y: -250}}
         animate={{ y: -10 }}
