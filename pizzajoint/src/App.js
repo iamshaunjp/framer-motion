db541f124a2067222477d9a20ddfbe4ab43eec6e
Cris Acosta <iamcris87@gmail.com>
2023-02-19 03:48:55 -0800

    update react-router-dom to v6. changed Switch to Routes

--- pizzajoint/src/App.js
+++ pizzajoint/src/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import { Route, Switch } from "react-router-dom";
-import Header from './components/Header';
-import Home from './components/Home';
+import { Routes, Route } from 'react-router-dom'
+
+import Home from './pages/Home';
 import Base from './components/Base';
 import Toppings from './components/Toppings';
 import Order from './components/Order';
+import Layout from './pages/Layout';
 
 function App() {
   const [pizza, setPizza] = useState({ base: "", toppings: [] });
@@ -25,21 +26,14 @@ function App() {
 
   return (
     <>
-      <Header />
-      <Switch>
-        <Route path="/base">
-          <Base addBase={addBase} pizza={pizza} />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />}/>
+          <Route path='/base' element={<Base addBase={addBase} pizza={pizza} />}/>
+          <Route path="/toppings" element={<Toppings addTopping={addTopping} pizza={pizza} />} />
+          <Route path="/order" element={<Order pizza={pizza} />} />
         </Route>
-        <Route path="/toppings">
-          <Toppings addTopping={addTopping} pizza={pizza} />
-        </Route>
-        <Route path="/order">
-          <Order pizza={pizza} />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
+      </Routes>
     </>
   );
 }
